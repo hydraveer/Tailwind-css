f02f013e703074db9a3a0bc54927167989c5d95d
Virendra Patel <85815897+hydraveer@users.noreply.github.com>
2023-10-29 20:28:06 +0530

    Login form created using tailwind css framework.
    I am just learning tailwind CSS, so for practice, I created this reusable form

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontfamily:['Rubik', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
+
